e6f805b96f2b068fb91622d181fa004197aa5d38
Ryo Komiyama <ryokomy@kyuzan.com>
2024-07-23 10:47:03 +0900

    Add deploy script

--- scripts/deployCounter.ts
+++ scripts/deployCounter.ts
@@ -1,13 +1,57 @@
-import { toNano } from '@ton/core';
-import { Counter } from '../wrappers/Counter';
-import { compile, NetworkProvider } from '@ton/blueprint';
+import * as fs from 'fs';
+import { getHttpEndpoint } from '@orbs-network/ton-access';
+import { mnemonicToWalletKey } from 'ton-crypto';
+import { TonClient, Cell, WalletContractV4 } from '@ton/ton';
+import Counter from '../wrappers/Counter'; // this is the interface class from step 7
+import dotenv from 'dotenv';
+dotenv.config();
 
-export async function run(provider: NetworkProvider) {
-    const counter = provider.open(Counter.createFromConfig({}, await compile('Counter')));
+export async function run() {
+    // initialize ton rpc client on testnet
+    const endpoint = await getHttpEndpoint({ network: 'testnet' });
+    const client = new TonClient({ endpoint });
 
-    await counter.sendDeploy(provider.sender(), toNano('0.05'));
+    // prepare Counter's initial code and data cells for deployment
+    const counterCode = Cell.fromBoc(fs.readFileSync('build/counter.cell'))[0]; // compilation output from step 6
+    const initialCounterValue = Date.now(); // to avoid collisions use current number of milliseconds since epoch as initial value
+    const counter = Counter.createForDeploy(counterCode, initialCounterValue);
 
-    await provider.waitForDeploy(counter.address);
+    // exit if contract is already deployed
+    console.log('contract address:', counter.address.toString());
+    if (await client.isContractDeployed(counter.address)) {
+        return console.log('Counter already deployed');
+    }
 
-    // run methods on `counter`
+    // open wallet v4 (notice the correct wallet version here)
+    const mnemonic = process.env.MNEMONIC; // your 24 secret words
+    if (!mnemonic) {
+        throw new Error('MNEMONIC is not set');
+    }
+    const key = await mnemonicToWalletKey(mnemonic.split(' '));
+    const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0 });
+    if (!(await client.isContractDeployed(wallet.address))) {
+        return console.log('wallet is not deployed');
+    }
+
+    // open wallet and read the current seqno of the wallet
+    const walletContract = client.open(wallet);
+    const walletSender = walletContract.sender(key.secretKey);
+    const seqno = await walletContract.getSeqno();
+
+    // send the deploy transaction
+    const counterContract = client.open(counter);
+    await counterContract.sendDeploy(walletSender);
+
+    // wait until confirmed
+    let currentSeqno = seqno;
+    while (currentSeqno == seqno) {
+        console.log('waiting for deploy transaction to confirm...');
+        await sleep(1500);
+        currentSeqno = await walletContract.getSeqno();
+    }
+    console.log('deploy transaction confirmed!');
+}
+
+function sleep(ms: number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- wrappers/Counter.ts
+++ wrappers/Counter.ts
@@ -1,29 +1,27 @@
-import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
+import { Contract, ContractProvider, Sender, Address, Cell, contractAddress, beginCell } from '@ton/core';
 
-export type CounterConfig = {};
-
-export function counterConfigToCell(config: CounterConfig): Cell {
-    return beginCell().endCell();
-}
-
-export class Counter implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
-
-    static createFromAddress(address: Address) {
-        return new Counter(address);
+export default class Counter implements Contract {
+    static createForDeploy(code: Cell, initialCounterValue: number): Counter {
+        const data = beginCell().storeUint(initialCounterValue, 64).endCell();
+        const workchain = 0; // deploy to workchain 0
+        const address = contractAddress(workchain, { code, data });
+        return new Counter(address, { code, data });
     }
 
-    static createFromConfig(config: CounterConfig, code: Cell, workchain = 0) {
-        const data = counterConfigToCell(config);
-        const init = { code, data };
-        return new Counter(contractAddress(workchain, init), init);
-    }
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    async sendDeploy(provider: ContractProvider, via: Sender) {
         await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
+            value: '0.01', // send 0.01 TON to contract for rent
+            bounce: false,
         });
+    }
+
+    async getCounter(provider: ContractProvider) {
+        const { stack } = await provider.get('counter', []);
+        return stack.readBigNumber();
     }
 }
